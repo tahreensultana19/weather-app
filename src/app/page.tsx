685a27c31f669ed983915552465e45a222656fe4
hardeep <hardeepdhariwal04@gmail.com>
2024-12-08 19:04:11 -0500

    Set up basic structure for the weather app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,13 @@
-//Home Page
+"use client";
+import React from "react";
+import "./globals.css";
+
+const Home: React.FC = () => {
+  return (
+    <main className="flex flex-col justify-center items-center bg-gradient-to-r from-blue-500 to-indigo-900 w-full h-[100vh]">
+      <h1 className="text-white text-4xl">Weather App</h1>
+    </main>
+  );
+};
+
+export default Home;
