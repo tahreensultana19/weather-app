0bc9f6e9d0d15e47305512369f23a3c4635d3a99
tahreen <tahreensultana19@gmail.com>
2024-12-08 19:14:13 -0500

    added date function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,14 @@
 import React from "react";
 import "./globals.css";
 
+function getCurrentDate(): string {
+    const currentDate = new Date();
+    const options: Intl.DateTimeFormatOptions = { month: "long" };
+    const monthName = currentDate.toLocaleString("en-US", options);
+    const date = new Date().getDate() + ", " + monthName;
+    return date;
+  }
+
 const Home: React.FC = () => {
   return (
     <main className="flex flex-col justify-center items-center bg-gradient-to-r from-blue-500 to-indigo-900 w-full h-[100vh]">
