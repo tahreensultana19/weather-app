88281dbcb8fc04f561b402d8f9f496633d9c201a
Nandani <singlanandani65@gmail.com>
2024-12-09 20:08:00 -0500

    added route file

---
+++ src/app/api/weather/route.tsx
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: { url: string | URL }) {
+  const { searchParams } = new URL(request.url);
+  const address = searchParams.get("address");
+  const latitude = searchParams.get("lat");
+  const longitude = searchParams.get("lon");
+  let url = "";
+  if (address) {
+    url =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      address +
+      "&appid=" +
+      "806e87305b8051479bb8950d8430d6ca";
+  } else {
+    url = https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=806e87305b8051479bb8950d8430d6ca;
+  }
+  console.log(url);
+  const res = await fetch(url);
+
+  const data = await res.json();
+  return NextResponse.json({ data });
+}
